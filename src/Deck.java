dfd9d001e315d4d0f8fd211dddc1cf9a63fafbc8
Jafar Farwaneh <jafarfarwana@gmail.com>
2024-02-03 05:16:40 +0300

    added methods to work with variable no. of players

--- src/Deck.java
+++ src/Deck.java
@@ -18,7 +18,7 @@ public class Deck {
             instance = new Deck();
         return instance;
     }
-    public void initializeCards(){
+    public void initializeCards(int noOfCards){
 
         for(int i = 2 ; i <= 10 ; i++){
             cards.add(new NumeralCard(Color.BLACK, Suit.SPADES,i));
@@ -33,12 +33,13 @@ public class Deck {
                 cards.add(new RankedCard(Color.BLACK,Suit.CLUBS,rank));
                 cards.add(new RankedCard(Color.RED,Suit.DIAMONDS,rank));
                 cards.add(new RankedCard(Color.RED,Suit.HEARTS,rank));
-
             }
         }
-
+        int excessCards = 52 - calculateNeededCards(noOfCards);
+        while(excessCards-- > 0){
+            cards.remove(cards.size()-1);
+        }
         cards.add(new RankedCard(Color.NONE,Suit.JOKER,Rank.Joker));
-
 
     }
 
@@ -48,5 +49,15 @@ public class Deck {
 
     public Card getTopCard(){
         return cards.remove(cards.size()-1);
+    }
+    public int calculateNeededCards(int noOfPlayers){
+        int excessCards = 52 % noOfPlayers;
+        if(excessCards % 2 != 0)
+            excessCards--;
+
+        return  52 - excessCards;
+    }
+    public int getNoOfCards(){
+        return cards.size();
     }
 }
