1751de6acd835c788f7ec6a802357fc57d5077b7
Jafar Farwaneh <jafarfarwana@gmail.com>
2024-02-03 05:16:02 +0300

    Added custom no. of Players input

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class Game extends Thread{
 
@@ -13,17 +11,21 @@ public class Game extends Thread{
     public void run() {
         GameManager gameManager = GameManager.getInstance();
 
+        System.out.println("Please enter the number of players (2-10)");
+        int noOfPlayers = readIntegerInput(2,10);
+
         List<Player> players = new ArrayList<>();
-        for(int i = 1 ; i < 5 ; i++){
-            players.add(new Player("P"+i, gameManager));
+        for(int i = 1 ; i <= noOfPlayers ; i++){
+            players.add(new Player("Player "+i, gameManager));
         }
         gameManager.initializeTurnQueue(players);
-        deck.initializeCards();
+        deck.initializeCards(noOfPlayers);
         deck.shuffle();
 
         dealCards(players,deck);
         System.out.println("------------ Game started ------------");
         System.out.println("No. of players : " + gameManager.getNoOfPlayers());
+        System.out.println("No. of Cards : " + (deck.calculateNeededCards(noOfPlayers)+1));
         System.out.println("First ,discarding matching cards simultaneously, then start taking turns  : ");
         for(Player player : players){
             player.start();
@@ -33,19 +35,45 @@ public class Game extends Thread{
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        System.out.println("------------ Game Over ------------");
-        System.out.println("The Old Maid is "+ gameManager.getLoser().getName());
+        System.out.println("\n------------ Game Over ------------");
+
+        System.out.println("***********************************");
+        System.out.println("***********************************");
+        System.out.println("**    The Old Maid is "+ gameManager.getLoser().getName()+"   **");
+        System.out.println("***********************************");
+        System.out.println("***********************************");
+
     }
 
     public void dealCards(List<Player> players , Deck deck){
-        // could have just given every player 13 cards at once,
-        // but I want to imitate the real game as much as possible (1 card each at a time)
-        for(int i = 0 ; i < 13 ; i++){
-            for(Player player : players){
+        int noOfCards = deck.calculateNeededCards(players.size());
+        int cardsPerPlayer = noOfCards / players.size();
+        for(Player player : players){
+            for(int i = 0 ; i < cardsPerPlayer; i++){
                 player.getHand().addToHand(deck.getTopCard());
             }
         }
+        if (players.size() * cardsPerPlayer < noOfCards)
+            players.get(players.size()-1).getHand().addToHand(deck.getTopCard());
         Random random = new Random();
         players.get(random.nextInt(players.size())).getHand().addToHand(deck.getTopCard());
+    }
+    public final int readIntegerInput(int min , int max){
+        Scanner scanner = new Scanner(System.in);
+        int input ;
+        while(true) {
+            try {
+                input = scanner.nextInt();
+                if (input >= min && input <= max)
+                    break;
+                else
+                    System.out.println("Please enter a number in the specified range("+min+"-"+max+")");
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input format , please enter a number :");
+            }finally {
+                scanner.nextLine();
+            }
+        }
+        return input;
     }
 }
